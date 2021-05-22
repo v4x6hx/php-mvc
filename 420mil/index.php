7e5aaf104da54acfddd39244be66f8f5367d5661
v4x6hx <abdielvasto@gmail.com>
2021-05-22 16:20:23 -0500

    Add existing file

---
+++ 420mil/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'controladores/ejercicios.controlador.php';
+require_once 'controladores/plantilla.controlador.php';
+$plantilla = new ControladorPlantilla();
+$plantilla -> CargarPlantilla();
