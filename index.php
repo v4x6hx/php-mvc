957becc8e5f0d9ed405ace83c43f80148569d359
v4x6hx <77085451+v4x6hx@users.noreply.github.com>
2021-08-13 19:57:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-
+<?php
+require_once 'controladores/ejercicios.controlador.php';
+require_once 'controladores/plantilla.controlador.php';
+$plantilla = new ControladorPlantilla();
+$plantilla -> CargarPlantilla();
+?>
